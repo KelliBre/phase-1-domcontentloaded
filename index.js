720e4299c70f0b453e3ba722f41a4a76a25c2f89
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-29 11:53:36 +1000

    commit tree 4ecf439e4bbbf49093a737798fafd8f624dd7628

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    replaceText ()
+
+});
+
+function replaceText () {
+    let newText = document.getElementById("text");
+    newText.textContent = "This is really cool!"
+}
